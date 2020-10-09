2bd9f9576421e82d684e155bd24feaaa7cf1d676
Marko Ristin <marko.ristin@gmail.com>
2020-10-09 21:57:45 +0200

    Bumped to 2.0.1 (#22)
    * Updated asttokens dependency range to >=2,<3 since it conflicted with icontract>=2.3.0

--- sphinx_icontract_meta.py
+++ sphinx_icontract_meta.py
@@ -3,7 +3,7 @@
 __title__ = 'sphinx-icontract'
 __description__ = 'Extend sphinx to include icontracts.'
 __url__ = 'https://github.com/Parquery/sphinx-icontract'
-__version__ = '2.0.0'
+__version__ = '2.0.1'
 __author__ = 'Marko Ristin'
 __author_email__ = 'marko.ristin@gmail.com'
 __license__ = 'MIT'
